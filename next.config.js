86cb32ef6d6eac8772651cc9a8f1271b1a239b3d
martinsvarc <77626343+martinsvarc@users.noreply.github.com>
2024-11-18 01:18:16 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  output: 'standalone',
+  reactStrictMode: true,
+  swcMinify: true,
+}
+
 module.exports = nextConfig
