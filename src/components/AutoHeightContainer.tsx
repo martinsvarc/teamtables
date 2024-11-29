605e02c355de2e220f91051c4eaa8cd122e9ed39
martinsvarc <77626343+martinsvarc@users.noreply.github.com>
2024-11-29 07:26:28 +0100

    Update AutoHeightContainer.tsx

--- src/components/AutoHeightContainer.tsx
+++ src/components/AutoHeightContainer.tsx
@@ -33,7 +33,7 @@ const AutoHeightContainer: React.FC<Props> = ({ children }) => {
     setTimeout(() => {
       isAdjustingRef.current = false;
       clearInterval(interval);
-    }, 3000);
+    }, 1000);
 
     return () => {
       window.removeEventListener('resize', handleResize);
